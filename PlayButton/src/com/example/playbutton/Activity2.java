ca838ab2985a7444a6a25b6bd5fb06d70f9b48f4
Sharniece <slvaughan2011@gmail.com>
2014-09-18 15:14:47 -0400

    First comit

---
+++ PlayButton/src/com/example/playbutton/Activity2.java
@@ -0,0 +1,11 @@
+package com.example.playbutton;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity{
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity2);
+	}
+
+}
---
+++ PlayButton/src/com/example/playbutton/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.playbutton;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button button1 = (Button)findViewById(R.id.button1);
+		Button button2 = (Button)findViewById(R.id.button2);
+		Button button3 = (Button)findViewById(R.id.button3);
+		
+		button1.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				Intent intent= new Intent(v.getContext(),Activity2.class);
+				startActivityForResult(intent,0);
+			}
+		});
+		//addButtonClickListener();
+	}
+}
+
