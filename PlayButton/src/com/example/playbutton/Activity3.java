a5bee0a31713614aea75cca656dc0da5e286b9dc
mdoweary <mdoweary@gmail.com>
2014-10-30 16:11:34 -0400

    dwf
    erf

--- PlayButton/src/com/example/playbutton/Activity3.java
+++ PlayButton/src/com/example/playbutton/Activity3.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -19,14 +22,14 @@ public class Activity3 extends Activity{
 		
 		addButtonListener();
 	}
-	
 	public void addButtonListener() {
 		image = (ImageView) findViewById(R.id.imageView1);
 		button = (Button) findViewById(R.id.button1);
 		image.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				image.setImageResource(R.drawable.iberty);
+				image.setImageResource(R.drawable.iberty);				
+				
 				/*Intent intent= new Intent(getActivity(),Activity3.class);
 				startActivity(intent,0);*/
 			}
